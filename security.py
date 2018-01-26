65ae60dfebd771cae9c6f00e0bdb93d805a8414c
Ivor Zhou <ivorz@sg.ibm.com>
2018-01-27 03:12:44 +0800

    utf8

--- security.py
+++ security.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import rsa
 
 pubkey, privkey = rsa.newkeys(512)
@@ -7,8 +8,14 @@ def get_public_key():
 
 def encrypt(message, keydata):
     key = rsa.PublicKey.load_pkcs1(keydata)
-    return rsa.encrypt(message.encode('utf8'), key)
+    return rsa.encrypt(message, key)
 
 def decrypt(message):
-    return rsa.decrypt(message, privkey).decode('utf8')
+    return rsa.decrypt(message, privkey)
 
+# s = "中文"
+# t = encrypt(s, get_public_key())
+# print t
+# d = decrypt(t)
+# print '-'
+# print d
